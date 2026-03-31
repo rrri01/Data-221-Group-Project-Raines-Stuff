0dccaa65628198c0c08e11437aa91c797a2c32d1
rrri01 <rraine001@gmail.com>
2026-03-30 18:19:39 -0600

    changed some comments so it looks more presentable

--- Neural Network.py
+++ Neural Network.py
@@ -1,5 +1,3 @@
-# github repo link for my own reference: https://github.com/rrri01/DATA-221-Group-Project-Raines-Stuff
-# group repository link for my own reference: https://github.com/chloeptrsn/DATA-221-Project.git
 
 import pandas as pd
 from sklearn.model_selection import train_test_split
@@ -34,12 +32,7 @@ target_prices = california_house_prices["median_house_value"]
 # splits the data into training data and testing data
 features_train, features_test, labels_train, labels_test = train_test_split(feature_matrix, target_prices, test_size=0.3, random_state=42)
 
-# scale data:
-# scaling = StandardScaler()
-# features_train = scaling.fit_transform(features_train)
-# features_test = scaling.transform(features_test)
-
-# neural network code and then print results
+# constructing the model
 tf.random.set_seed(1)
 neural_network_model = Sequential()
 
@@ -56,22 +49,24 @@ neural_network_model.add(third_hidden_layer)
 output_layer = Dense(1)
 neural_network_model.add(output_layer)
 
-# compile the model
+# compiling the model
 neural_network_model.compile(loss='mean_absolute_error')
 
-# training and implementation
+# training and implementing the model
 neural_network_model.fit(feature_matrix, target_prices, epochs=10)
 
-predicted_labels = neural_network_model.predict(features_test, verbose = 1)
+predicted_labels = neural_network_model.predict(features_test)
 
+# finding the mean absolute error
 test_mae = neural_network_model.evaluate(features_test, labels_test)
 train_mae = neural_network_model.evaluate(features_train, labels_train)
+
+# finding the root mean squared error and r2 score
+rmse = root_mean_squared_error(labels_test, predicted_labels)
+r2 = r2_score(labels_test, predicted_labels)
 
 print(f"Testing Mean Absolute Error: {test_mae}")
 print(f"Training Mean Absolute Error: {train_mae}")
 
-# root mean squared error
-rmse = root_mean_squared_error(labels_test, predicted_labels)
-r2 = r2_score(labels_test, predicted_labels)
 print(f"RMSE: {rmse}")
 print(f"R2 SCORE: {r2}")
